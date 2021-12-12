1cb58cafc3b2a8dec784eb9a50ea5414c36b7555
eyalgi <eyalg125@gmail.com>
2021-12-12 10:28:22 +0200

    removed lm

--- ex3_307887984_307830901.py
+++ ex3_307887984_307830901.py
@@ -17,7 +17,6 @@ from dython import nominal
 from sklearn.preprocessing import MinMaxScaler
 import copy
 import math
-import language_model
 
 
 # nltk.download('vader_lexicon')
@@ -28,7 +27,6 @@ import language_model
 ########################################################################################################################
 # --------------------------------------- Data Understanding & Pre Processing ----------------------------------------#
 ########################################################################################################################
-
 
 def load_best_model():
     pass
@@ -100,8 +98,6 @@ def preliminary_feature_extraction(df):
     publish_time_feature(pfe_df)
     # #### Pos Tag feature: ####
     pos_tag_feature(pfe_df)
-
-    lm_eval_feature(pfe_df)
 
     return pfe_df
 
@@ -281,26 +277,6 @@ def pos_tag_feature(df):
     return df
 
 
-def lm_eval_feature(df):
-    """
-    Creates a features based on the Score from a language model which is based on the Train Trumps' tweets.
-    Every tweet is evaluated by the lm, and the log probability is added as the feature value.
-    :param df: DataFrame
-    :return: the df with the new feature
-    """
-    trump_tweets = list(df.loc[df.label == 0, 'tweet'])
-    text = " ".join(trump_tweets)
-    nt = language_model.normalize_text(text)  # lower casing, padding punctuation with white spaces
-    lm = language_model.Ngram_Language_Model(n=3, chars=False)
-    lm.build_model(nt)  #
-    scores = []
-    for t in df['tweet']:
-        scores.append(lm.evaluate(t))
-
-    df['lm_evaluation'] = scores
-    return df
-
-
 def feature_understanding(df):
     """
     Visualise the features to better understand the data.
@@ -308,7 +284,7 @@ def feature_understanding(df):
     """
     pd.options.display.max_columns = 10
     pd.options.display.width = 1000
-    features = ['lm_evaluation', 'tags_count', 'hashtags_count', 'quotes', 'url', 'written_time', 'ex_mark',
+    features = ['tags_count', 'hashtags_count', 'quotes', 'url', 'written_time', 'ex_mark',
                 'tag_realDonaldTrump',
                 'full_cap_words_count', 'cap_words_count', 'negative_score', 'tweet_length', 'hr_publish_time', 'NN',
                 'DT', 'IN', 'JJ', 'NNS', 'VBZ', 'VBD']
@@ -330,8 +306,7 @@ def features_plots(df, col_name):
     # ---------------------#
     if col_name == 'tags_count' or col_name == 'hashtags_count' or col_name == 'ex_mark' or col_name == 'negative_score' \
             or col_name == 'tweet_length' or col_name == 'hr_publish_time' or col_name == 'NN' or col_name == 'DT' \
-            or col_name == 'IN' or col_name == 'JJ' or col_name == 'NNS' or col_name == 'VBZ' or col_name == 'VBD' \
-            or col_name == 'lm_evaluation':
+            or col_name == 'IN' or col_name == 'JJ' or col_name == 'NNS' or col_name == 'VBZ' or col_name == 'VBD':
         sns.boxplot(x='label', y=col_name, data=m_df, palette='Set2', showmeans=True,
                     meanprops={"marker": "o", "markerfacecolor": "white", "markeredgecolor": "black"})
         plt.title(col_name)
@@ -435,9 +410,9 @@ def feature_correlation(df):
     :param df: DataFrame
     :return: Heatmap of correlations
     """
-    temp_df = df
+    temp_df = copy.deepcopy(df)
     temp_df = temp_df.drop(['id', 'user', 'tweet', 'time', 'device'],
-                           axis=1)  # drop not relevant featurs before make a heatmap of correlations
+                           axis=1)  # drop not relevant features before make a heatmap of correlations
     nominal.associations(temp_df, nominal_columns='all')
 
 
@@ -449,15 +424,16 @@ def pre_process_main():
     train_data = read_data('trump_train.tsv')  # read the data
     train_data_fe = preliminary_feature_extraction(train_data)  # feature extraction
     # feature_understanding(train_data_fe)
-    normalize_features(train_data_fe, ['hashtags_count' , 'tweet_length' , 'tags_count', 'lm_evaluation']) # normalized the features to be between [0,1].
+    normalize_features(train_data_fe,
+                       ['hashtags_count', 'tweet_length', 'tags_count'])  # normalized the features to be between [0,1].
     train_data_fe = make_categorized_from_hr_publish(train_data_fe)
-    train_data_fe = pd.get_dummies(train_data_fe, columns =['hr_publish_time'], drop_first=True)
+    train_data_fe = pd.get_dummies(train_data_fe, columns=['hr_publish_time'], drop_first=True)
     # pd.set_option('display.max_columns' , None)
     # print(train_data_fe)
 
     ######################################## for correlation ##########################################################
-    train_data_fe=set_type_for_features(train_data_fe) # make the type boolean for the correlation heatmap
-    feature_correlation(train_data_fe) #make a heatmap correlation
+    # train_data_fe = set_type_for_features(train_data_fe)  # make the type boolean for the correlation heatmap
+    # feature_correlation(train_data_fe)  # make a heatmap correlation
     ###################################################################################################################
     # pre_processing() (feature_selection) #chage name to make_final_data or something like that
 
@@ -467,7 +443,9 @@ def pre_process_main():
     Y_train = temp_df['label']
     x_train, x_test_check, y_train, y_test_check = train_test_split(X_train, Y_train, test_size=0.1,
                                                                     random_state=42, stratify=Y_train)
+
     pass
+
 
 ########################################################################################################################
 # ----------------------------------------------------- Models -------------------------------------------------------#
