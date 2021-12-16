440669aebc3675887e5fe0065850d34c527f4bf4
Eyal <eyalgi@post.bgu.ac.il>
2021-12-16 22:58:51 +0200

    finished ann class

--- save_for_later.py
+++ save_for_later.py
@@ -37,7 +37,7 @@ class ConvertDataset(Dataset):
 
 
 class NN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, input_size, hidden_size, num_classes, dropout_p):
         super(NN, self).__init__()
         # Fully connected layers
         self.fc1 = nn.Linear(input_size, hidden_size[0])
@@ -45,7 +45,7 @@ class NN(nn.Module):
         self.fc_out = nn.Linear(hidden_size[1], num_classes)
         # Activation, dropout, batch-normalization layers
         self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.3)
+        self.dropout = nn.Dropout(p=dropout_p)
         self.batchnorm1 = nn.BatchNorm1d(hidden_size[0])
         self.batchnorm2 = nn.BatchNorm1d(hidden_size[1])
 
@@ -55,8 +55,7 @@ class NN(nn.Module):
         x = self.relu(self.fc2(x))
         # x = self.batchnorm2(x)
         # x = self.dropout(x)
-        x = self.fc_out(x)
-        out = torch.sigmoid(x)
+        out = self.fc_out(x)
 
         return out
 
@@ -112,7 +111,7 @@ class NN(nn.Module):
         return history
 
     def _binary_acc(self, y_pred, y_test):
-        y_pred_tag = torch.round(y_pred)
+        y_pred_tag = torch.round(torch.sigmoid(y_pred))
 
         correct_results_sum = (y_pred_tag == y_test).sum().float()
         acc = correct_results_sum / y_test.shape[0]
@@ -124,14 +123,42 @@ class NN(nn.Module):
         # evaluation
         self.eval()
         train_predicted = self(torch.tensor(x_train, dtype=torch.float32))
-        train_predicted = train_predicted
+        train_predicted = torch.sigmoid(train_predicted)
         train_acc = (train_predicted.reshape(-1).detach().numpy().round() == y_train).mean()
 
         test_predicted = self(torch.tensor(x_test, dtype=torch.float32))
-        test_predicted = test_predicted
+        test_predicted = torch.sigmoid(test_predicted)
         test_acc = (test_predicted.reshape(-1).detach().numpy().round() == y_test).mean()
 
+        # print(f'train accuracy = {train_acc:.3f}, test accuracy = {test_acc:.3f}')
+
         return train_acc, test_acc
+
+    # def evaluate(self, train_loader, test_loader, y_test):
+    #     # evaluation
+    #     y_pred_list = []
+    #     train_y_pred_list = []
+    #     self.eval()
+    #     with torch.no_grad():
+    #         for X_batch in test_loader:
+    #             X_batch = X_batch.to(device)
+    #             y_test_pred = self(X_batch)
+    #             y_test_pred = torch.sigmoid(y_test_pred)
+    #             y_pred_tag = torch.round(y_test_pred)
+    #             y_pred_list.append(y_pred_tag.cpu().numpy())
+    #
+    #     with torch.no_grad():
+    #         for X_batch, y_batch in train_loader:
+    #             X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+    #             y_train_pred = self(X_batch)
+    #             train_acc = self._binary_acc(y_train_pred, y_batch.unsqueeze(1))
+    #
+    #     y_pred_list = [a.squeeze().tolist() for a in y_pred_list]
+    #     test_acc = (y_pred_list == y_test).sum().item() / len(test_loader.dataset)
+    #
+    #     print(f'train accuracy = {train_acc:.3f}, test accuracy = {test_acc:.3f}')
+    #
+    #     return train_acc, test_acc
 
     def plot_acc_loss(self, history_dict):
         # accuracy plot
@@ -152,7 +179,21 @@ class NN(nn.Module):
         plt.show()
 
 
-def kfold_tuning(X, y, params):
+# # ###########____________________________________________________________________###################################
+
+# Model's Parameters
+input_size_ = 15
+hidden_size_ = [16, 16]
+num_classes_ = 1
+num_epochs_ = 16
+batch_size_ = 128
+learning_rate_ = 0.1
+
+# Load Datasets
+X_train, Y_train, X_test = ex3.read_and_split_data()
+
+
+def ann_tuning(X, y, params):
     skf = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
     tuning_params = []
     tuning_train_acc = []
@@ -164,21 +205,22 @@ def kfold_tuning(X, y, params):
     print(f'{options} Options -> {10 * options} iterations')
 
     param_values = [v for v in params.values()]
-    i = 1
-    for i_s, h_s, e, b_s, lr in product(*param_values):
+    for i_s, h_s, c, e, b_s, lr, d_p in product(*param_values):
         print(
-            f'{i}/{options} Tuning parameters-> input_size:{i_s} | hidden_size:{h_s} | epochs:{e} | batch_size:{b_s} | learning_rate:{lr}'
+            f'Tuning parameters-> input_size:{i_s} | hidden_size:{h_s} | epochs:{e} | batch_size:{b_s} | learning_rate:{lr} | dropout_p:{d_p}'
         )
-        i += 1
-        nn_clf = NN(input_size=i_s, hidden_size=h_s, num_classes=1).to(device)
+
+        nn_clf = NN(input_size=i_s, hidden_size=h_s, num_classes=c, dropout_p=d_p).to(device)
         cv_train_acc = []
+        cv_train_loss = []
         cv_val_acc = []
+        cv_val_loss = []
         for train_index, test_index in skf.split(X, y):
             x_train, x_val = X.iloc[train_index], X.iloc[test_index]
             y_train, y_val = y.iloc[train_index], y.iloc[test_index]
             train_set, val_set = prepare_datasets(x_train, y_train, x_val, b_s)
 
-            criterion = nn.BCELoss()
+            criterion = nn.BCEWithLogitsLoss()
             optimizer = optim.Adam(nn_clf.parameters(), lr=lr)
 
             nn_clf.fit(train_loader=train_set,
@@ -191,8 +233,8 @@ def kfold_tuning(X, y, params):
             cv_train_acc.append(acc[0])
             cv_val_acc.append(acc[1])
         print(f'train_acc: {np.mean(cv_train_acc):.3f}, val_acc:{np.mean(cv_val_acc):.3f}')
-        iter_params = {'input_size': i_s, 'hidden_size': h_s, 'epochs': e,
-                       'batch_size': b_s, 'learning_rate': lr}
+        iter_params = {'input_size': i_s, 'hidden_size': h_s, 'classes': c, 'epochs': e,
+                       'batch_size': b_s, 'learning_rate': lr, 'dropout_p': d_p}
         tuning_params.append(iter_params)
         tuning_val_acc.append(np.mean(cv_val_acc))
         tuning_train_acc.append(np.mean(cv_train_acc))
@@ -220,20 +262,75 @@ def prepare_datasets(x_train, y_train, x_validation, batch_size):
     return train_loader, validation_loader
 
 
-def ann_tuning(x_train, y_train, params_grid):
-    results = kfold_tuning(X=x_train, y=y_train, params=params_grid)
-    results = pd.DataFrame(results).sort_values('mean_test_score', ascending=False)
-    headers = ['Parameters', 'Validation score', 'Train score']
-    print(tabulate(results.head(10), headers=headers, tablefmt='grid'))
-
-
-# Load Datasets
-X_train, Y_train, X_test = ex3.read_and_split_data()
+params_grid = {'INPUT_SIZE': [X_train.shape[1]],
+               'HIDDEN_SIZE': [[16, 16], [32, 32], [64, 64], [128, 128], [256, 256]],
+               'CLASSES': [1],
+               'EPOCHS': [8, 16, 32, 64],
+               'BATCH_SIZE': [16, 32, 64, 128],
+               'LR': [0.001, 0.01, 0.1],
+               'DROPOUT_P': [0.1, ]
+               }
 
 params_grid = {'INPUT_SIZE': [X_train.shape[1]],
-               'HIDDEN_SIZE': [[16, 16], [32, 32], [64, 64], [128, 128], [16, 8], [32, 16], [64, 32], [128, 64]],
-               'EPOCHS': [4, 8, 16, 32],
-               'BATCH_SIZE': [16, 32, 64, 128],
-               'LR': [0.001, 0.01, 0.1]
+               'HIDDEN_SIZE': [[16, 16], [32, 32]],
+               'CLASSES': [1],
+               'EPOCHS': [8, 16],
+               'BATCH_SIZE': [128],
+               'LR': [0.1],
+               'DROPOUT_P': [0.1]
                }
-ann_tuning(x_train=X_train, y_train=Y_train, params_grid=params_grid)
+
+# results = ann_tuning(X=X_train, y=Y_train, params=params_grid)
+# results = pd.DataFrame(results).sort_values('mean_test_score', ascending=False)
+# headers = ['Parameters', 'Validation score', 'Train score']
+# print(tabulate(results.head(), headers=headers, tablefmt='grid'))
+
+# ####################################################################################################################
+# ####################################################################################################################
+# ####################################################################################################################
+# ####################################################################################################################
+# # ###########____________________________________________________________________###################################
+x_train_, x_validation_, y_train_, y_validation_ = train_test_split(X_train, Y_train, test_size=0.1, random_state=42)
+
+x_train_ = x_train_.to_numpy()
+x_validation_ = x_validation_.to_numpy()
+y_train_ = y_train_.to_numpy()
+y_validation_ = y_validation_.to_numpy()
+
+#  Convert datasets
+train_dataset_ = ConvertDataset(x=torch.FloatTensor(x_train_), y=torch.FloatTensor(y_train_), train=True)
+validation_dataset_ = ConvertDataset(x=torch.FloatTensor(x_validation_), y=torch.FloatTensor(y_validation_), train=True)
+validation_test_dataset_ = ConvertDataset(x=torch.FloatTensor(x_validation_), train=False)
+# test_dataset = ConvertDataset(x=X_test.values)
+
+# Create DataLoaders
+train_loader_ = DataLoader(dataset=train_dataset_, batch_size=batch_size_, shuffle=True)
+validation_loader_ = DataLoader(dataset=validation_dataset_, batch_size=batch_size_, shuffle=True)
+validation_test_loader_ = DataLoader(dataset=validation_test_dataset_, batch_size=1, shuffle=False)
+# # todo: test_loader?
+# # ###########____________________________________________________________________###################################
+#
+#
+# # ###########____________________________________________________________________###################################
+# Initiate model
+nn_model = NN(input_size=input_size_, hidden_size=hidden_size_, num_classes=num_classes_, dropout_p=0.3).to(device)
+
+# loss and optimizer
+criterion_ = nn.BCEWithLogitsLoss()
+optimizer_ = optim.Adam(nn_model.parameters(), lr=learning_rate_)
+
+history_ = nn_model.fit(train_loader=train_loader_,
+                        criterion=criterion_,
+                        optimizer=optimizer_, epochs=num_epochs_,
+                        validation_loader=validation_loader_, verbose=1)
+# nn_model.evaluate(train_loader=train_loader_, test_loader=validation_test_loader_, y_test=y_validation_)
+print(nn_model.evaluate(x_train=x_train_, y_train=y_train_, x_test=x_validation_, y_test=y_validation_))
+#
+#
+# ########### report ###############
+# # print(confusion_matrix(y_validation, y_pred_list))
+# # print(classification_report(y_validation, y_pred_list))
+# #         n_samples += labels.shape[0]
+# #         n_correct += (outputs == labels).sum().item()
+# #
+# # ###########____________________________________________________________________###################################
